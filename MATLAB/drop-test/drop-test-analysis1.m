1c488b5ad672b8e58c6bbca59666ec143ddc84b2
sisi <sisi.chen1@gmail.com>
2015-07-06 16:55:16 -0700

    add drop-test and in-drop folders

---
+++ MATLAB/drop-test/drop-test-analysis1.m
@@ -0,0 +1,38 @@
+%% input data from hundred stamps, mouse: 
+
+%% Initialize variables.
+filename = '/home/iamcam/Documents/cell-seq/MATLAB/drop-test/GSM1544799_SpeciesMix_HundredSTAMPs_MOUSE.digital_expression.csv';
+delimiter = '\t';
+startRow = 2;
+
+%% Format string for each line of text:
+ 
+formatSpec = '%*s%*s%s%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(filename,'r');
+
+%% Read columns of data according to format string.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+
+%% Post processing for unimportable data.
+% No unimportable data rules were applied during the import, so no post
+% processing code is included. To generate code which works for
+% unimportable data, select unimportable cells in a file and regenerate the
+% script.
+
+%% Create output variable
+m_datam = [dataArray{2:end-1}];
+m_genes = dataArray{1};
+
+%% Clear out unused data
+clear dataArray fileID formatSpec delimter startRow
+
+%% Remove zero read genes: 
+
